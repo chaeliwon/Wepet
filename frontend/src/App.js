7c2eda148c64a1e862be27723f5bd35e9f08dd60
earthnine <bople1126@gmail.com>
2024-10-15 12:06:20 +0900

    1015 12:06 지현
    메인화면에 로고이미지 추가 / 슬라이드 추가

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,14 @@ import React from 'react';
 import Homepage from './components/Homepage';
 import './App.css';
 
+
 function App() {
   return (
+    
     <div className="App">
       <div className="mobile-container">
         <Homepage />
+    
       </div>
     </div>
   );
--- frontend/src/components/Homepage.jsx
+++ frontend/src/components/Homepage.jsx
@@ -1,12 +1,33 @@
 import React from 'react';
 import './css/Homepage.css';
+import logo from '../assets/WePetLogo.png';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/effect-cards';
+import { EffectCards } from 'swiper/modules';
 
 const Homepage = () => {
   return (
     <div className="homepage-background">
-      
+      <img src={logo} alt="We Pet Logo" className="logo" />
+      <Swiper
+        effect={'cards'}
+        grabCursor={true}
+        modules={[EffectCards]}
+        className="mySwiper"
+      >
+        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+        <SwiperSlide>Slide 5</SwiperSlide>
+        <SwiperSlide>Slide 6</SwiperSlide>
+        {/* <SwiperSlide>Slide 7</SwiperSlide>
+        <SwiperSlide>Slide 8</SwiperSlide>
+        <SwiperSlide>Slide 9</SwiperSlide> */}
+      </Swiper>
     </div>
-  )
+  );
 }
 
-export default Homepage
+export default Homepage;
