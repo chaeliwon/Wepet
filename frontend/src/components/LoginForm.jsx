88d500c516424843c8b17d08dc9d748ee80b7bb5
earthnine <bople1126@gmail.com>
2024-10-15 17:11:28 +0900

    1015 5:11 로그인페이지 수정

--- frontend/src/components/LoginForm.jsx
+++ frontend/src/components/LoginForm.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './css/LoginForm.css';
 
 const LoginForm = () => {
@@ -6,11 +7,13 @@ const LoginForm = () => {
     <div className="login-container">
       <h1>로 그 인</h1>
       <form>
-        <label htmlFor="username">아이디</label>
-        <input type="text" id="username" name="username" required />
+        <label htmlFor="useremail">이메일</label>
+        <input type="text" id="useremail" name="useremail" required />
         
         <label htmlFor="password">비밀번호</label>
         <input type="password" id="password" name="password" required />
+        
+        <Link to="/password-reset" className="find-link">아이디/비밀번호 찾기</Link> {/* 여기에 추가 */}
         
         <button type="submit" className="login-btn">로그인🥑</button>
       </form>
@@ -19,4 +22,3 @@ const LoginForm = () => {
 }
 
 export default LoginForm;
- 
