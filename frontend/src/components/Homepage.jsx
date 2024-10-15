3560f0e7ef407d778b1f83259ccd42afe63f2843
earthnine <bople1126@gmail.com>
2024-10-15 14:49:20 +0900

    1015 2:48 지현
    메인 화면 버튼 추가 / 슬라이드 추가 / 귀여운 강아지 추가

--- frontend/src/components/Homepage.jsx
+++ frontend/src/components/Homepage.jsx
@@ -1,6 +1,10 @@
+// Homepage.jsx
 import React from 'react';
 import './css/Homepage.css';
 import logo from '../assets/WePetLogo.png';
+import jelly from '../assets/jelly.png';
+import chatbot from '../assets/chatbot.png';
+import slidedog1 from '../assets/slidedog1.jpg'; // 경로 수정
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/effect-cards';
@@ -10,22 +14,39 @@ const Homepage = () => {
   return (
     <div className="homepage-background">
       <img src={logo} alt="We Pet Logo" className="logo" />
-      <Swiper
-        effect={'cards'}
-        grabCursor={true}
-        modules={[EffectCards]}
-        className="mySwiper"
-      >
-        <SwiperSlide>Slide 1</SwiperSlide>
+      
+      {/* 상담하기 버튼 및 챗봇 버튼 */}
+      <div className="consultation-container">
+        <button className="chatbot-btn">
+          <img src={chatbot} alt="Chatbot" className="icon" />
+        </button>
+        <button className="consultation-btn">
+          상담 하기
+          <img src={jelly} alt="paw" className="icon" />
+        </button>
+      </div>
+      
+      <Swiper effect={'cards'} grabCursor={true} modules={[EffectCards]} className="mySwiper">
+        <SwiperSlide>
+          <img src={slidedog1} alt="Dog 1" style={{ width: '100%', height: '100%', objectFit: 'cover', borderRadius: '18px' }} />
+        </SwiperSlide>
         <SwiperSlide>Slide 2</SwiperSlide>
         <SwiperSlide>Slide 3</SwiperSlide>
         <SwiperSlide>Slide 4</SwiperSlide>
         <SwiperSlide>Slide 5</SwiperSlide>
-        <SwiperSlide>Slide 6</SwiperSlide>
-        {/* <SwiperSlide>Slide 7</SwiperSlide>
-        <SwiperSlide>Slide 8</SwiperSlide>
-        <SwiperSlide>Slide 9</SwiperSlide> */}
       </Swiper>
+      
+      {/* 로그인 및 찜 목록 버튼 */}
+      <div className="bottom-buttons">
+        <button className="bottom-btn">
+          로그인
+          <img src={jelly} alt="paw" className="icon" />
+        </button>
+        <button className="bottom-btn">
+          찜 목록
+          <img src={jelly} alt="paw" className="icon" />
+        </button>
+      </div>
     </div>
   );
 }
