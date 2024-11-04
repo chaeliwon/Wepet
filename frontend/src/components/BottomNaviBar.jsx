d443e1f1636a09ceaf48c4158036abc94d8c6822
hbjoo96 <hbjoo9606@gmail.com>
2024-11-04 12:32:51 +0900

    페이지 이동 시 하단네비바 버튼 사진 변경
    페이지 이동 시 하단네비바 버튼 사진 변경

--- frontend/src/components/BottomNaviBar.jsx
+++ frontend/src/components/BottomNaviBar.jsx
@@ -1,18 +1,39 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const BottomNaviBar = () => {
+  const location = useLocation();
+  const getIconSrc = (path) => {
+    switch (path) {
+      case "/liked":
+        return location.pathname === "/liked"
+          ? "/static/LikeOn.png"
+          : "/static/LikeOff.png";
+      case "/":
+        return location.pathname === "/"
+          ? "/static/HomeOn.png"
+          : "/static/HomeOff.png";
+      case "/mypage":
+        return location.pathname === "/mypage"
+          ? "/static/MyOn.png"
+          : "/static/Myoff.png";
+    }
+  };
   return (
     <div className="naviBar">
       <Link to="liked">
         <div className="naviContainer">
-          <img className="naviIcon" src="/static/Like.png" alt="찜 아이콘" />
+          <img
+            className="naviIcon"
+            src={getIconSrc("/liked")}
+            alt="찜 아이콘"
+          />
           <span>찜</span>
         </div>
       </Link>
       <Link to="/">
         <div className="naviContainer">
-          <img className="naviIcon" src="/static/Home.png" alt="홈 아이콘" />
+          <img className="naviIcon" src={getIconSrc("/")} alt="홈 아이콘" />
           <span>홈</span>
         </div>
       </Link>
@@ -20,7 +41,7 @@ const BottomNaviBar = () => {
         <div className="naviContainer">
           <img
             className="naviIcon"
-            src="/static/MyPage.png"
+            src={getIconSrc("/mypage")}
             alt="마이페이지 아이콘"
           />
           <span>마이</span>
